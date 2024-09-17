dab1a2e3d89a2623d246ca10411564dedeb870e0
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-09-16 21:04:25 -0300

    First commit - Inicialização do projeto Mimikus

---
+++ src/main/java/com/github/marciokleber/mimikus/MimikusApplication.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.mimikus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimikusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MimikusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/marciokleber/mimikus/MimikusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.mimikus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimikusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
